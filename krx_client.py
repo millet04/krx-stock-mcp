45dcad704920c2d9ef8663f26c6b89bb539f2a9c
millet04 <minseok04@lgcns.com>
2026-02-07 15:02:16 +0900

    [feat] 동기 방식의 API 호출 함수 추가

--- krx_client.py
+++ krx_client.py
@@ -1,13 +1,12 @@
 import os
-import sys
 import aiohttp
-import logging
-from typing import Dict, Any, Optional
-
+import requests
+from typing import Dict, Any
+from utils import LOGGER
 
 class KrxStockClient:
 
-    async def make_request(self, url: str) -> Optional[Dict[str, Any]]:
+    async def make_request(self, url: str) -> Dict[str, Any]:
         headers = {}
         if api_key := os.environ.get("KRX_API_KEY"):
             headers["AUTH_KEY"] = api_key
@@ -18,7 +17,21 @@ class KrxStockClient:
                     response.raise_for_status()
                     return await response.json()
             except Exception as e:
-                return {"Error": str(e)}
+                LOGGER.exception(f"[KRX API] API request failed.Check if the url is valid: {url}")
+                return {}
+        
+
+    def make_request_sync(self, url: str) -> Dict[str, Any]:
+        headers = {}
+        if api_key := os.environ.get("KRX_API_KEY"):
+            headers["AUTH_KEY"] = api_key
+        try:
+            response = requests.get(url, headers=headers, timeout=5.0)
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            LOGGER.exception(f"[KRX API] API request failed. Check if the url is valid: {url}")
+            return {}
 
 
     async def fetch_stock_info(
@@ -32,17 +45,32 @@ class KrxStockClient:
         
         url = f"http://data-dbg.krx.co.kr/svc/apis/sto/{market}_isu_base_info?basDd={date}"
         data = await self.make_request(url)
-        if not data:
-            raise RuntimeError(f"API request failed.Check if the url is valid: {url}")
         
         records = data.get("OutBlock_1", [])
         if not records:
-            raise RuntimeError(
-                f"No market data found from API. Check if the date ({date}) or company information is valid."
-            )
-        
+            LOGGER.error(f"[KRX API] No market data found from API. Check if the date ({date}) is valid.")
+
         return {record['ISU_NM']:record for record in records}
         
+
+    async def fetch_stock_info_sync(
+        self,
+        date: str,
+        market: str
+    ) -> Dict[str, Dict[str, Any]]:
+        """Request stock inofrmation from API"""
+        if market not in ["stk", "ksq", "knx"]:
+            raise ValueError("Market must be the one of 'stk', 'ksq', or 'knx'.")
+        
+        url = f"http://data-dbg.krx.co.kr/svc/apis/sto/{market}_isu_base_info?basDd={date}"
+        data = await self.make_request_sync(url)
+        
+        records = data.get("OutBlock_1", [])
+        if not records:
+            LOGGER.error(f"[KRX API] No market data found from API. Check if the date ({date}) is valid.")
+                    
+        return {record['ISU_NM']:record for record in records}
+
 
     async def fetch_stock_price(
         self,
@@ -55,13 +83,28 @@ class KrxStockClient:
         
         url = f"http://data-dbg.krx.co.kr/svc/apis/sto/{market}_bydd_trd?basDd={date}"
         data = await self.make_request(url)
-        if not data:
-            raise RuntimeError(f"API request failed.Check if the url is valid: {url}")
         
         records = data.get("OutBlock_1", [])
         if not records:
-            raise RuntimeError(
-                f"No market data found from API. Check if the date ({date}) or company information is valid."
-            )
+            LOGGER.error(f"[KRX API] No market data found from API. Check if the date ({date}) is valid.")
         
-        return {record['ISU_NM']:record for record in records} 
+        return {record['ISU_NM']:record for record in records}
+
+
+    async def fetch_stock_price_sync(
+        self,
+        date: str,
+        market: str
+    ) -> Dict[str, Dict[str, Any]]:
+        """Request stock price from API"""
+        if market not in ["stk", "ksq", "knx"]:
+            raise ValueError("Market must be the one of 'stk', 'ksq', or 'knx'.")
+        
+        url = f"http://data-dbg.krx.co.kr/svc/apis/sto/{market}_bydd_trd?basDd={date}"
+        data = await self.make_request_sync(url)
+        
+        records = data.get("OutBlock_1", [])
+        if not records:
+            LOGGER.error(f"[KRX API] No market data found from API. Check if the date ({date}) is valid.")
+        
+        return {record['ISU_NM']:record for record in records}  
